09c58df978a7d8bddbbf9215782f37bf24911b30
Emerald <brightonlcox@gmail.com>
2023-09-28 17:29:27 -0600

    Delete tui-helper-proc-macro/target directory

--- tui-helper-proc-macro/target/debug/build/proc-macro2-ba6ebaab9df6f488/build_script_build-ba6ebaab9df6f488.d
+++
@@ -1,5 +0,0 @@
-/Users/brightoncox/Programming/rust/libs/tui-helper/tui-helper-proc-macro/target/debug/build/proc-macro2-ba6ebaab9df6f488/build_script_build-ba6ebaab9df6f488: /Users/brightoncox/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.67/build.rs
-
-/Users/brightoncox/Programming/rust/libs/tui-helper/tui-helper-proc-macro/target/debug/build/proc-macro2-ba6ebaab9df6f488/build_script_build-ba6ebaab9df6f488.d: /Users/brightoncox/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.67/build.rs
-
-/Users/brightoncox/.cargo/registry/src/index.crates.io-6f17d22bba15001f/proc-macro2-1.0.67/build.rs:
